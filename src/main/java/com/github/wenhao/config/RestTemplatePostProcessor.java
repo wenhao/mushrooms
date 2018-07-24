c14652d0cd0d5f3b0f7ba4507c350534a5274089
Wen Hao <wenhao@126.com>
2018-07-24 18:29:51 +0800

    [MOCK-2][文浩] 使用 restTemplate interceptor。

---
+++ src/main/java/com/github/wenhao/config/RestTemplatePostProcessor.java
@@ -0,0 +1,32 @@
+package com.github.wenhao.config;
+
+import com.github.wenhao.interceptor.ParrotCacheInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Configuration
+@ConditionalOnProperty(value = "parrot.enabled", havingValue = "true", matchIfMissing = true)
+@RequiredArgsConstructor
+public class RestTemplatePostProcessor implements BeanPostProcessor {
+
+    private final ParrotCacheInterceptor parrotCacheInterceptor;
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof RestTemplate) {
+            final RestTemplate restTemplate = (RestTemplate) bean;
+            List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+            interceptors.add(parrotCacheInterceptor);
+            restTemplate.setInterceptors(interceptors);
+            return restTemplate;
+        }
+        return bean;
+    }
+}
